395caf36185be05be3663d624d79a060b43fb5f8
senthil kumar <senthilkumar.r@skava.com>
2018-12-19 19:09:12 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -2,58 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header/Header'
+import {  Route,  Switch} from 'react-router-dom'
+import Home from './Home/Home'
 
-class App extends React.Component {
+class App extends Component {
  
-    // fires before component is mounted
-    constructor(props) {
- 
-        // makes this refer to this component
-        super(props);
- 
-        // set local state
-        this.state = {
-            date: new Date(),
-            showResults: false
-        };
- 
-    }
-
-    componentDidMount() {
-      this.timerId = setInterval( 
-        () => this.tick(),1000
-      )
-    }
-    componentWillUnmount() {
-      clearInterval(this.timerId);
-    }
-  
-    tick = () => {
-      this.setState({
-        date: new Date()
-      })
-    } 
-  
-    onClick = () => {
-      let isShow = true;
-      if(this.state.showResults === true) {
-        isShow = false;
-      } else {
-        isShow = true
-      }
-      this.setState({ showResults: isShow });
-      console.log(this.state.showResults)
-    } 
-
+   
     render() {
         return (
           <div>
             <Header />
-            <h1>
-              It is {this.state.date.toLocaleTimeString()}.
-            </h1>
-            <input type="submit" value="Search" onClick={this.onClick} />
-                { this.state.showResults ? "hi": null }
+            <Switch>
+              <Route path="/" exact={true} component={Home} />
+           </Switch>
+           
           </div>
 
         )
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,7 @@
 import React ,{Component} from 'react';
+import { BrowserRouter as Router, Route, NavLink, Switch, Redirect} from 'react-router-dom'
 import './Header.css';
+
 
 
 class Header extends Component {
@@ -7,28 +9,29 @@ class Header extends Component {
     render() {
         return (
             <header className="header">
-            <div className="headerContainer">
-                <div className="row">
-                    <div className="col-xl-3 col-6">
-                        <a href="/" className="logo">
-                        <img src="/images/site_logo.logo" alt="logo img"></img>
-                        </a>
+                <div className="headerContainer">
+                    <div className="row">
+                        <div className="col-xl-3 col-6">
+                            <a href="/" className="logo">
+                                <img src="/images/site_logo.logo" alt="logo img"></img>
+                            </a>
+                        </div>
+                        <div className="col-lg-6 hide-for-lg-down">
+                                <nav>
+                                    <ul id="menu-main" className="navbar-nav mr-auto mt-2 mt-lg-0 ">
+                                        <li  className="nav-item "><NavLink to='/'>Home</NavLink></li>
+                                        <li  className="nav-item "><NavLink to='/about'>About Us</NavLink></li>
+                                        <li  className="nav-item "><NavLink to='/agencies'>Agencies</NavLink></li>
+                                        <li  className="nav-item "><NavLink to='/contactUs'>Contact Us</NavLink></li>
+                                    </ul>
+                                    
+                                </nav>
+                            </div>        
                     </div>
-                    <div class="col-lg-6 hide-for-lg-down">
-                            <nav>
-                                <ul id="menu-main" class="navbar-nav mr-auto mt-2 mt-lg-0 ">
-                                <li  class=" nav-item "><a href="/" class="nav-link">Home</a></li>
-                                <li  class="  nav-item "><a href="https://sproutsend.com/features/" class="nav-link">Features</a></li>
-                                <li  class="  nav-item "><a href="https://sproutsend.com/agencies/" class="nav-link">Agencies</a></li>
-                                <li  class="  nav-item "><a href="https://sproutsend.com/contact-us/" class="nav-link">Contact Us</a></li>
-                                </ul>
-                            </nav>
-                        </div>        
                 </div>
-            
-            </div>
-            
             </header>
+
+            
         )            
     }
 }
